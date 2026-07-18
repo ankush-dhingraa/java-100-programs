860176a344806b539d75ac08f3f778f45aa8cdb9
ankush-dhingraa <ankushkumar20024@gmail.com>
2026-07-18 21:03:20 +0530

    use float format printf

--- ReadRadiusOfCircleAndPrintAreaCircumference.java
+++ ReadRadiusOfCircleAndPrintAreaCircumference.java
@@ -7,8 +7,8 @@ public class ReadRadiusOfCircleAndPrintAreaCircumference{
         double areaOfCircle = Math.PI * radiusOfCircle * radiusOfCircle;
         double circumferenceOfCircle = 2 * Math.PI * radiusOfCircle;
 
-        System.out.println("Area of circle with " + radiusOfCircle + " radius is : " + areaOfCircle);
-        System.out.println("Circumference of circle is : " + circumferenceOfCircle);
+        System.out.printf("Area of circle with " + radiusOfCircle + " radius is : %.2f%n", areaOfCircle);
+        System.out.printf("Circumference of circle is : %.2f%n", circumferenceOfCircle);
 input.close();
     }
 }
