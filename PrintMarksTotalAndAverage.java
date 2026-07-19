c2aabd14a9eaf256934c80d852d8dea7520098e5
ankush-dhingraa <ankushkumar20024@gmail.com>
2026-07-19 12:33:13 +0530

    read subject marks using loop

--- PrintMarksTotalAndAverage.java
+++ PrintMarksTotalAndAverage.java
@@ -5,28 +5,18 @@ import java.util.*;
 public class PrintMarksTotalAndAverage {
     public static void main(String[] args) {
         try(Scanner input = new Scanner(System.in)){
+            int totalSubjects = 5;
+            float totalMarks = 0;
             System.out.println("Please enter your five subject marks ==>");
-            float subject1;
-            float subject2;
-            float subject3;
-            float subject4;
-            float subject5;
-            System.out.print("Subject 1 : ");
-            subject1 = input.nextFloat();
-            System.out.print("Subject 2 : ");
-            subject2 = input.nextFloat();
-            System.out.print("Subject 3 : ");
-            subject3 = input.nextFloat();
-            System.out.print("Subject 4 : ");
-            subject4 = input.nextFloat();
-            System.out.print("Subject 5 : ");
-            subject5 = input.nextFloat();
+            for(int i = 1;i <=totalSubjects;i++){
+                System.out.print("Subject " + i + " : ");
+                totalMarks += input.nextFloat();
 
-            //calculating total and average
-            float totalMarks = subject1 + subject2 + subject3 + subject4 + subject5;
-            float averageMarks = totalMarks / 5;
+            }
 
-            //display result
+            float averageMarks = totalMarks / totalSubjects;
+          
+            // //display result
             System.out.println("Total marks of five subject is : " + totalMarks);
             System.out.println("Average marks of fice subject is : " + averageMarks);
 
